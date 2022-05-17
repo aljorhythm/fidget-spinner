89b25a4ef9f3ff3ba510f0e0226b184c268c295f
Joel Lim <joel.lim@Joels-MacBook-Pro.local>
2022-05-17 13:49:19 +0800

    logic edit

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	let r = 0, g = 0, b = 0;
-	let rDelta = Math.round(Math.random() * 50);
-	let gDelta = Math.random() * 50;
-	let bDelta = Math.random() * 50;
+	let r = 0, g = 0, b = 0, a = 0;
+	let rDelta = Math.round(Math.random() * 20);
+	let gDelta = Math.random() * 20;
+	let bDelta = Math.random() * 20;
 	function handleKeydown(event) {
 		const keycode = event.keyCode;
-		r = (r + (event.keyCode) % rDelta) % 250;
-		g = (g + (event.keyCode) % gDelta) % 250;
-		b = (b + (event.keyCode) % bDelta) % 250;
-		document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+		r = (r + (event.keyCode % rDelta)) % 250;
+		g = (g + (event.keyCode % gDelta)) % 250;
+		b = (b + (event.keyCode % bDelta)) % 250;
+		document.body.style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
 	}
 </script>
 
